f79de202686d818d3a9a6270ee915c6a4e98d311
PatriDominguez <122823076+PatriDominguez@users.noreply.github.com>
2023-03-20 20:29:14 +0100

    Calcular mínimo array completado

---
+++ src/main/java/ieseuropa/ArrayMenor.java
@@ -0,0 +1,19 @@
+package main.java.ieseuropa;
+
+public class ArrayMenor {
+	
+	private static int calcularMinimo(Integer [] numerosArray) {
+		int menor = numerosArray[0];
+		for (int i=0; i<numerosArray.length; i++) {
+			if (numerosArray[i] < menor) {
+				menor = numerosArray[i];
+			}
+		}
+		return menor;
+	}
+
+	public static void main(String[] args) {
+		Integer [] numerosArray = {5, 7, 3, 20, 12, 22};
+		System.out.println("El numero menor es: " + calcularMinimo(numerosArray));
+	}
+}
