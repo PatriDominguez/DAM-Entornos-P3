84879afc46d500d6a264fe6f6e53f83b61678210
Jorge <Jorge@DESKTOP-IJOFI7U>
2023-03-21 15:39:14 +0100

    Método para ordenar de menor a mayor los números de un Array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,26 @@
 package main.java.ieseuropa;
 
 public class Array {
+	private static void menorMayor(int[] numeros) {
+		for (int i = 0; i < numeros.length - 1; i++) {
+		    for (int j = 0; j < numeros.length - i - 1; j++) {
+		        if (numeros[j] > numeros[j + 1]) {
+		            int temp = numeros[j];
+		            numeros[j] = numeros[j + 1];
+		            numeros[j + 1] = temp;
+		        }
+		    }
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		int[] numeros ={2,10, 5, 27, 130, -1, 18, 93};
+		menorMayor(numeros);
+		for (int i = 0; i < numeros.length; i++) {
+		    System.out.print(numeros[i] + " ");
+		}
 	}
 
 }
