ef8853225c433ddce047367b74615ae45203da46
dev <dev@debian>
2023-03-22 02:16:58 +0100

    metodo para saber la longitud de un string.

---
+++ src/main/java/ieseuropa/Striing.java
@@ -0,0 +1,20 @@
+package main.java.ieseuropa;
+
+public class Striing {
+	
+	public static int longitud(String string) {
+
+		int size = string.length();
+
+		return size;
+		
+		
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
