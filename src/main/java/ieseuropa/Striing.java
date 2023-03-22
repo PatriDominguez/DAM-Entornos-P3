a75a2a00ab9ef05f0476f3c33a2fbda69211c046
dev <dev@debian>
2023-03-22 02:13:28 +0100

    metodos de pasar a miyusculas y minucusculas dado un string.

---
+++ src/main/java/ieseuropa/Striing.java
@@ -0,0 +1,24 @@
+package main.java.ieseuropa;
+
+public class Striing {
+	
+	public static String mayus(String string) {
+
+		return string = string.toUpperCase();
+
+	}
+
+	public static String minus(String string) {
+
+		return string = string.toLowerCase();
+
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
