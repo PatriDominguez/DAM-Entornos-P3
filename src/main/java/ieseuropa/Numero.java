9cc6e6e276700acfd711566f95c87bae70cb4c1a
dev <dev@debian>
2023-03-22 20:36:46 +0100

    metodo para saber la media de una array

--- src/main/java/ieseuropa/Numero.java
+++ src/main/java/ieseuropa/Numero.java
@@ -2,8 +2,24 @@ package main.java.ieseuropa;
 
 public class Numero {
 
+	public static void media(int[] numeros) {
+
+		int mayor = 0;
+		int suma = 0;
+		for (int i = 0; i < numeros.length; i++) {
+			if (numeros[i] > mayor) {
+				mayor = numeros[i];
+			}
+			suma += numeros[i];
+		}
+		System.out.println("La media es " + suma / numeros.length);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] numeros = { 1, 8, 8, 14, 32 };
+		
+		media(numeros);
 
 	}
 
