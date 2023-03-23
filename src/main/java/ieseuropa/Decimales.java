d4c742824b7ff78bffed571b41566a697edb9948
Jorge <Jorge@DESKTOP-IJOFI7U>
2023-03-23 11:49:21 +0100

    Método para calcular una ecuación de segundo grado

--- src/main/java/ieseuropa/Decimales.java
+++ src/main/java/ieseuropa/Decimales.java
@@ -1,5 +1,6 @@
 package main.java.ieseuropa;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Decimales {
@@ -9,11 +10,31 @@ public class Decimales {
 	private static int quitarDecimales(float num) {
 		return (int) num;
 	}
+	private static float[] equacion(float a, float b, float c) {
+		float discriminante = b*b-4*a*c;
+		float[] soluciones;
+		if(discriminante > 0) {
+			soluciones = new float[2];
+			soluciones[0] = (float) ((-b + Math.sqrt(discriminante))/(2*a));
+			soluciones[1] = (float) ((-b - Math.sqrt(discriminante))/(2*a));
+		} else if(discriminante == 0) {
+			soluciones = new float[1];
+			soluciones[0] = -b / (2*a);
+		} else {
+			soluciones = new float[2];
+			soluciones[0] = (-b) / (2*a);
+			soluciones[1] = (float) (Math.sqrt(-discriminante) / (2*a));
+		}
+		return soluciones;
+	}
 
 	public static void main(String[] args) {
 		float num = 3.956f;
 		System.out.println(redondarAlza(num));
 		System.out.println(quitarDecimales(num));
+		float a = 2, b = 10, c = -8;
+		float[] soluciones = equacion(a, b, c);
+		System.out.println(Arrays.toString(soluciones));
 	}
 
 }
