abebaf45462cff68e9b64d6020dfab1db3abbc68
Jorge <Jorge@DESKTOP-IJOFI7U>
2023-03-23 11:43:24 +0100

    Método para redondear al alza

--- src/main/java/ieseuropa/Decimales.java
+++ src/main/java/ieseuropa/Decimales.java
@@ -3,12 +3,16 @@ package main.java.ieseuropa;
 import java.util.Scanner;
 
 public class Decimales {
+	private static float redondarAlza(float num) {
+		return (float) Math.round(num);
+	}
 	private static int quitarDecimales(float num) {
 		return (int) num;
 	}
 
 	public static void main(String[] args) {
 		float num = 3.956f;
+		System.out.println(redondarAlza(num));
 		System.out.println(quitarDecimales(num));
 	}
 
