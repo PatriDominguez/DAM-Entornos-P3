061d26a78d1f9ae3c4b29a6a28b78f23e9307fb5
PatriDominguez <122823076+PatriDominguez@users.noreply.github.com>
2023-03-24 15:57:44 +0100

    Cambio de nombre de clases y completo clase arreglo con maximo

--- src/main/java/ieseuropa/Arreglo.java
+++ src/main/java/ieseuropa/Arreglo.java
@@ -1,8 +1,21 @@
 package main.java.ieseuropa;
 
-public class Arrayy {
-	private static void menorMayor(Integer[] numerosArray) {
-		Arrays.sort(numerosArray);
+import java.util.Arrays;
+
+public class Arreglo {
+	
+	private static int calcularMaximo(Integer [] numeros) {
+		int mayor = numeros[0];
+		for (int i=0; i<numeros.length; i++) {
+			if (numeros[i] > mayor) {
+				mayor = numeros[i];
+			}
+		} 
+		return mayor;
+	}
+	
+	private static void menorMayor(Integer[] numeros) {
+		Arrays.sort(numeros);
 	}
 
 	public static void media(Integer[] numeros) {
@@ -17,6 +30,7 @@ public class Arrayy {
 		}
 		System.out.println("La media es " + suma / numeros.length);
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer[] numeros = { 5, 7, 3, 20, 12, 22 };
@@ -24,6 +38,8 @@ public class Arrayy {
 		menorMayor(numeros);
 		System.out.println(Arrays.toString(numeros));
 		media(numeros);
+		
+		System.out.println("El numero mayor es: " + calcularMaximo(numeros));
 	}
 
 }
